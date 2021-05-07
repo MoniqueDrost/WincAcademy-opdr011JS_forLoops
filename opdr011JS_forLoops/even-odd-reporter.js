216a1bf2baca3b5dd493296e5966aefbf6182d27
MoniqueDrost <80634091+MoniqueDrost@users.noreply.github.com>
2021-05-07 14:46:45 +0200

    Add files via upload

---
+++ opdr011JS_forLoops/even-odd-reporter.js
@@ -0,0 +1,12 @@
+
+
+for (number = 0; number <= 20; number++) {
+    if (number % 2 === 0) {
+        n = "even";
+    }
+    else {
+        n = "odd";
+    }
+    console.log(number + " is " + n);
+}
+
---
+++ opdr011JS_forLoops/multiplication-tables.js
@@ -0,0 +1,31 @@
+
+/* for (number = 1; number <= 10; number++) {
+    const tableOutput1 = (number*9);
+    console.log(number + ' x 1 = ' + tableOutput1);
+
+} */
+
+
+for (number = 1; number <= 10; number++) {
+    const tableOutput1 = (1*number);
+    console.log(number + ' x 1 = ' + tableOutput1);
+    const tableOutput2 = (2*number);
+    console.log(number + ' x 2 = ' + tableOutput2);
+    const tableOutput3 = (3*number);
+    console.log(number + ' x 3 = ' + tableOutput3);
+    const tableOutput4 = (4*number);
+    console.log(number + ' x 4 = ' + tableOutput4);
+    const tableOutput5 = (5*number);
+    console.log(number + ' x 5 = ' + tableOutput5);
+    const tableOutput6 = (6*number);
+    console.log(number + ' x 6 = ' + tableOutput6);
+    const tableOutput7 = (7*number);
+    console.log(number + ' x 7 = ' + tableOutput7);
+    const tableOutput8 = (8*number);
+    console.log(number + ' x 8 = ' + tableOutput8);
+    const tableOutput9 = (9*number);
+    console.log(number + ' x 9 = ' + tableOutput9);
+    const tableOutput10 = (10*number);
+    console.log(number + ' x 10 = ' + tableOutput10);
+} 
+
